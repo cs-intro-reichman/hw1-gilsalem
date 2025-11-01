86c52fe45c01e51505233c734a4fc045505cd887
gilsalem <gil.salem@post.runi.ac.il>
2025-11-01 16:15:11 +0200

    finish

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,19 @@
 // Splits a restaurant bill evenly among three diners.
-public class Bill3 {
-	public static void main(String[] args) {
+public class Bill3 
+{
+	public static void main(String[] args)
+	 {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		int bill  = Integer.parseInt(args[3]);
+		double spiltbill=bill/3.0;
+		System.out.println("Dear "+name3+" , "+name2+" and " +name1+":pay"+ Math.ceil(spiltbill) );
+
+			
+
+	
 	    // Replace this comment with the rest of your code   
 	}
 }
--- PrintTo5.java
+++ PrintTo5.java
@@ -1,10 +1,26 @@
 // Prints the numbers 0 to 5.
 // (Same program as shown in lecture 1-1).
+
+
+
 public class PrintTo5 {
-	public static void main(String[] args) {
-		// Declares an int variable, and sets it to 1
-        int i = 5;
-        // Complete the program's code here:
-        System.out.println(i);
+	public static void main(String[] args)
+     {
+        
+		int lim=Integer.parseInt((args[0]));
+        int num1=((int) (Math.random()*lim));
+        int num2=((int) (Math.random()*lim));
+        int num3=((int) (Math.random()*lim));
+        System.out.println(num1+","+num2+","+num3);
+        int max=Math.max(num2, num3);
+        max=Math.max(max,num2);
+        int min=Math.min(num1,num2);
+        min=Math.min(min,num3);
+        int mid=(num1+num2+num3)-(max-min);
+        System.out.println(max+","+mid+","+min);
+            
+
+
+
     }
  }
